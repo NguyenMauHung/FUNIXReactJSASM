93431cbeb4e303da4759a26053bf85768e48d2e7
Nguyen Mau Hung <hungnmfx14861@funix.edu.vn>
2022-02-28 16:01:35 +0700

    5 commit : add event OnClick

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -5,17 +5,36 @@ class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      selectedStaff: null
     }
   }
 
+  onStaffSelect(staff) {
+    this.setState({ selectedStaff: staff })
+  }
 
-
+  renderStaff(staff) {
+    if (staff != null) {
+      return (
+        <Card>
+          <CardImg width="100%" src={staff.image} alt={staff.name} />
+          <CardBody>
+            <CardTitle>{staff.name}</CardTitle>
+            <CardText>{staff.doB}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return (
+        <div></div>
+      );
+    }
+  }
   render() {
     const menu = this.props.staffs.map(staff => {
       return (
         <div key={staff.id} className="col-12 col-md-5 m-1">
-          <Card>
+          <Card onClick={() => this.onStaffSelect(staff)}>
             <CardImg width="100%" src={staff.image} alt={staff.name} />
             <CardImgOverlay>
               <CardTitle>{staff.name}</CardTitle>
@@ -28,6 +47,9 @@ class Menu extends Component {
       <div className="container">
         <div className="row">
           {menu}
+        </div>
+        <div className="row">
+          {this.renderStaff(this.state.selectedStaff)}
         </div>
       </div>
     );
