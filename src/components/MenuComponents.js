7a83b0964369fa46d7230779c933e2c6b52645ed
Nguyen Mau Hung <hungnmfx14861@funix.edu.vn>
2022-02-28 16:23:41 +0700

    6 commit : change interface

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+// import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
 
 class Menu extends Component {
   constructor(props) {
@@ -14,15 +14,17 @@ class Menu extends Component {
   }
 
   renderStaff(staff) {
+
     if (staff != null) {
       return (
-        <Card>
-          <CardImg width="100%" src={staff.image} alt={staff.name} />
-          <CardBody>
-            <CardTitle>{staff.name}</CardTitle>
-            <CardText>{staff.doB}</CardText>
-          </CardBody>
-        </Card>
+        <div className="col-12 m-3">
+          <h4>Họ và tên: {staff.name}</h4>
+          <p>Ngày sinh: {staff.doB}</p>
+          <p>Ngày vào công ty: {staff.startDate}</p>
+          {/* <p>Phòng ban: {staff.department}</p> */}
+          <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
+          <p>Số ngày đã làm thêm: {staff.overTime}</p>
+        </div>
       );
     } else {
       return (
@@ -33,13 +35,10 @@ class Menu extends Component {
   render() {
     const menu = this.props.staffs.map(staff => {
       return (
-        <div key={staff.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.onStaffSelect(staff)}>
-            <CardImg width="100%" src={staff.image} alt={staff.name} />
-            <CardImgOverlay>
-              <CardTitle>{staff.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
+        <div key={staff.id} className="col-12 col-md-5 m-3">
+          <div onClick={() => this.onStaffSelect(staff)}>
+            <div>{staff.name}</div>
+          </div>
         </div>
       );
     });
@@ -47,6 +46,7 @@ class Menu extends Component {
       <div className="container">
         <div className="row">
           {menu}
+          <div>Bấm vào tên nhân viên để xem thông tin</div>
         </div>
         <div className="row">
           {this.renderStaff(this.state.selectedStaff)}
