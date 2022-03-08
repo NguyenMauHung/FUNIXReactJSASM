85ab1a3fdb7ba7524a6efbcf2ac9632ec3cf3831
Nguyen Mau Hung <hungnmfx14861@funix.edu.vn>
2022-03-08 23:03:47 +0700

    commit 2 : add Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponents'
-import { DEPARTMENTS, ROLE, STAFFS } from './shared/staffs'
+import Main from './components/MainComponent';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      departments: DEPARTMENTS,
-      role: ROLE,
-      staffs: STAFFS,
-    };
-  }
+function App() {
 
-
-
-  render() {
-    return (
-      <div>
-        <Navbar dark color="primary">
-          <div className='container'>
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự 1.0</NavbarBrand>
-          </div>
-        </Navbar>
-
-        <Menu staffs={this.state.staffs} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './FooterComponent';
+import Header from './HeaderComponents';
+
+function Main() {
+
+    return (
+        <div className="App">
+            <Header />
+            <Footer />
+
+        </div>
+    );
+}
+
+export default Main;
